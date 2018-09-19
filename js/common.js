aaa3054cdedad07b4e5b6ff1efc01015193fd935
FrankYeah <aaa24295234@gmail.com>
2018-09-19 18:11:22 +0800

    feature

--- js/common.js
+++ js/common.js
@@ -9,22 +9,56 @@ const header = document.getElementById('header');
 
 window.addEventListener('scroll',winScroll);
 function winScroll(){
-    if(document.documentElement.scrollTop>100 && document.documentElement.scrollTop<150){
-        console.log('123')
+    if(document.documentElement.scrollTop>100){
         webSeach.src = "";
         logo.src = "images/FE_logo-4.png";
-        headerP1.style.color="rgb(128,128,128)"
-        headerP2.style.color="rgb(128,128,128)"
-        headerP3.style.color="rgb(128,128,128)"
+        headerP1.style.color="rgb(128,128,128)";headerP3.style.color="rgb(128,128,128)";headerP2.style.color="rgb(128,128,128)";
         header.style.backgroundColor="white";
       }else if(document.documentElement.scrollTop<100){
-        console.log('333')
         webSeach.src = "images/FE_search.png";
         logo.src = "images/FE_logo-1.png";
-        headerP1.style.color="white"
-        headerP2.style.color="white"
-        headerP3.style.color="white"
+        headerP1.style.color="white";headerP3.style.color="white";headerP2.style.color="white";
         header.style.backgroundColor="";
       }
 }
 
+// back to top
+
+window.onload = function() {
+
+    let top = document.getElementById('tops');
+    let timer = null;
+    let isTop = true;
+
+    top.onclick = function(){
+        //设置定时器
+        timer = setInterval(function(){
+            //获取滚动条距离顶部的高度
+            let osTop = document.documentElement.scrollTop || document.body.scrollTop ;  //同时兼容了ie和Chrome浏览器
+                
+            //减小的速度
+            let isSpeed = Math.floor(-osTop / 6);
+            document.documentElement.scrollTop = document.body.scrollTop = osTop + isSpeed;
+            //console.log( osTop + isSpeed);
+
+            isTop = true;
+
+            //判断，然后清除定时器
+            if (osTop == 0) {
+                clearInterval(timer);
+            }
+        },30);
+    };  
+
+
+}
+
+// 遊戲上下跳動
+
+const testGo = document.getElementById('testGo');
+window.addEventListener('click',upAndDown)
+
+function upAndDown(){
+    console.log('hi')
+    header.style.animation = "myfirst 2s infinite";
+}
