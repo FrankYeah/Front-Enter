125f4350bae3f2c6b9c1fff2f69c4bcea9569ae0
FrankYeah <aaa24295234@gmail.com>
2018-09-21 13:35:51 +0800

    article layout

--- js/article.js
+++ js/article.js
@@ -18,3 +18,26 @@ function changeKevisual(){
     if(i>=keyvisualImg_len) { i=0;}
 
 }
+
+// 回調
+
+// var funcA = function(callback){
+//     var i = 3;
+  
+//     setTimeout(function(){
+//       console.log('function A');
+//         setTimeout(console.log("2"),2000)
+//         callback();
+//     }, i * 1000);
+//   };
+  
+//   var funcB = function(){
+//       var i = 3;
+  
+//       setTimeout(function(){
+//         console.log('function B');
+//       }, i * 1000);
+//   };
+  
+//   // 將 funcB 作為參數帶入 funcA()
+//   funcA( funcB );
