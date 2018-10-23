9802ae3273c89bfc98d261727a42d7fe02e17708
FrankYeah <aaa24295234@gmail.com>
2018-10-23 16:27:40 +0800

    fun refactor 1

--- js/content.js
+++ js/content.js
@@ -12,115 +12,52 @@ getAllData.orderByChild('creatTime').equalTo(Number(articleId)).on('child_added'
 function createLayout(data){
     app.get('#section').style.background  =  "url('" + data.rectangleUrl + "') 50% / cover no-repeat"; 
     app.get('#keyvisualSpan').textContent = data.name;
-    app.createElement("div", {atrs:{
-        className:'main-chart',
-        id: 'mainChart'
-    }}, app.get('#mainId'));
-
-    imageLeftOne();
-    imageLeftTwo();
-    imageLeftThree();
-    imageLeftFour();
-    imageLeftFive();
-
-    mainContentBorn();
-    mainContentTitleBorn(data);
-    coreContentBorn(data);
-    boxTitle(data);
-    mainUnderlineBorn();
-    boxTable();
-    boxtableOne();
-    tableOneLeft();
-    tableOneRight(data);
-    boxtableTwo();
-    tableTwoLeft();
-    tableTwoRight(data);
-    boxtableThree();
-    tableThreeLeft();
-    tableThreeRight(data);
-    boxtableFour();
-    tableFourLeft();
-    tableFourRight(data);
-    boxtableFive();
-    tableFiveLeft();
-    tableFiveRight(data);
-    boxtableSix();
-    tableSixLeft();
-    tableSixRight(data);
-    boxtableSeven();
-    tableSevenLeft();
-    tableSevenRight(data);
-    boxtableEight();
-    tableEightLeft();
-    tableEightRight(data);
+    app.get('#mainContentTitle').textContent = data.topic;
+    app.get('#mainContentText').innerHTML = data.content;
+    app.get('#rightCity').innerHTML = data.city;
+    app.get('#rightClass').innerHTML = data.classType;
+    app.get('#rightTeach').innerHTML = data.teachWay;
+    app.get('#rightDay').innerHTML = data.totalDay + '天';
+    app.get('#rightWeek').innerHTML = data.weekHour + '小時';
+    app.get('#rightTech').innerHTML = data.technology;
+    app.get('#rightMail').innerHTML = data.mail;
+    app.get('#rightPhone').innerHTML = data.phone;
 }
 
-function imageLeftOne(data){
-    app.createElement("div", {atrs:{
-        className:'image-left image-left-one',
-    }, evts:{
-        click:app.evts.clickSize
-    }}, app.get('#mainChart'));
+app.get('#imageLeftOne').addEventListener('click', function(){
+    rotateRotate = 0;
+    // let oneUrl = '../images/2.jpg';
+    let oneUrl = '../Front-Enter/images/2.jpg';
+    rotateImg(oneUrl);
+})
 
+app.get('#imageLeftTwo').addEventListener('click', function(){
+    rotateRotate = 1;
+    // let twoUrl = '../images/13.jpg';
+    let twoUrl = '../Front-Enter/images/13.jpg';
+    rotateImg(twoUrl);
+})
 
-    let newElement = document.createElement('div');
-    newElement.className = 'image-left image-left-one';
-    document.getElementById('mainChart').appendChild(newElement);
-    newElement.onclick = function(){
-        rotateRotate = 0;
-        // let oneUrl = '../images/2.jpg';
-        let oneUrl = '../Front-Enter/images/2.jpg';
-        rotateImg(oneUrl);
-    }
-}
+app.get('#imageLeftThree').addEventListener('click', function(){
+    rotateRotate = 2; 
+    // let threeUrl = '../images/15.jpg';
+    let threeUrl = '../Front-Enter/images/15.jpg';
+    rotateImg(threeUrl);
+})
 
-function imageLeftTwo(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'image-left image-left-two';
-    document.getElementById('mainChart').appendChild(newElement);
-    newElement.onclick = function(){
-        rotateRotate = 1;
-        // let twoUrl = '../images/13.jpg';
-        let twoUrl = '../Front-Enter/images/13.jpg';
-        rotateImg(twoUrl);
-    }
-}
+app.get('#imageLeftFour').addEventListener('click', function(){
+    rotateRotate = 3; 
+    // let fourUrl = '../images/7.jpg';
+    let fourUrl = '../Front-Enter/images/7.jpg';
+    rotateImg(fourUrl);
+})
 
-function imageLeftThree(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'image-left image-left-three';
-    document.getElementById('mainChart').appendChild(newElement);
-    newElement.onclick = function(){
-        rotateRotate = 2; 
-        // let threeUrl = '../images/15.jpg';
-        let threeUrl = '../Front-Enter/images/15.jpg';
-        rotateImg(threeUrl);
-    }
-}
-
-function imageLeftFour(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'image-left image-left-four';
-    document.getElementById('mainChart').appendChild(newElement);
-    newElement.onclick = function(){
-        rotateRotate = 3; 
-        // let fourUrl = '../images/7.jpg';
-        let fourUrl = '../Front-Enter/images/7.jpg';
-        rotateImg(fourUrl);
-    }
-}
-
-function imageLeftFive(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'image-left image-left-five';
-    document.getElementById('mainChart').appendChild(newElement);
-    newElement.onclick = function(){
-        rotateRotate = 4; 
-        // let fiveUrl = '../images/AppWorksShool-rectangle.jpg';
-        let fiveUrl = '../Front-Enter/images/AppWorksShool-rectangle.jpg';
-        rotateImg(fiveUrl);
-    }
-}
+app.get('#imageLeftFive').addEventListener('click', function(){
+    rotateRotate = 4; 
+    // let fiveUrl = '../images/AppWorksShool-rectangle.jpg';
+    let fiveUrl = '../Front-Enter/images/AppWorksShool-rectangle.jpg';
+    rotateImg(fiveUrl);
+})
 
 function rotateImg(url){
     rotateBackFull();
@@ -195,7 +132,6 @@ function startLeft(){
     app.get("#rotateCenterImg").style.backgroundRepeat  =  'no-repeat';  
     app.get("#rotateCenterImg").style.backgroundSize  =  'cover';
     app.get("#rotateCenterImg").style.backgroundPositionX = 'center';
-
 }
 
 function startRight(){
@@ -225,232 +161,6 @@ function deleteRotateFull(){
     document.body.removeChild(anotherChild);
 }
 
-// right
-
-function mainContentBorn(){
-    let newElement = document.createElement('div');
-    newElement.id = 'mainContent';
-    newElement.className = 'main-content';
-    document.getElementById('mainId').appendChild(newElement);
-}
-
-function mainContentTitleBorn(data){
-    let newElement = document.createElement('p');
-    newElement.className = 'main-content-title';
-    newElement.textContent = data.topic;
-    document.getElementById('mainContent').appendChild(newElement);
-}
-
-function coreContentBorn(data){
-    let newElement = document.createElement('p');
-    newElement.innerHTML = data.content;
-    document.getElementById('mainContent').appendChild(newElement);
-}
-
-function boxTitle(data){
-    let newElement = document.createElement('p');
-    newElement.innerHTML = '整理';
-    newElement.className = 'box-title';
-    document.getElementById('mainContent').appendChild(newElement);
-}
-
-function mainUnderlineBorn(){
-    let newElement = document.createElement('div');
-    newElement.className = 'main-underline';
-    document.getElementById('mainContent').appendChild(newElement);
-}
-
-function boxTable(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table';
-    newElement.id = 'boxTable';
-    document.getElementById('mainContent').appendChild(newElement);
-}
-
-function boxtableOne(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table-one';
-    newElement.id = 'boxtableOne';
-    document.getElementById('boxTable').appendChild(newElement);
-}
-
-function tableOneLeft(){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-left';
-    newElement.textContent = '城市';
-    newElement.id = 'tableOneLeft';
-    document.getElementById('boxtableOne').appendChild(newElement);
-}
-
-function tableOneRight(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-right';
-    newElement.textContent = data.city;
-    newElement.id = 'tableOneRight';
-    document.getElementById('boxtableOne').appendChild(newElement);
-}
-
-function boxtableTwo(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table-one';
-    newElement.id = 'boxtableTwo';
-    document.getElementById('boxTable').appendChild(newElement);
-}
-
-function tableTwoLeft(){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-left';
-    newElement.textContent = '班制';
-    newElement.id = 'tableTwoLeft';
-    document.getElementById('boxtableTwo').appendChild(newElement);
-}
-
-function tableTwoRight(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-right';
-    newElement.textContent = data.classType;
-    newElement.id = 'tableTwoRight';
-    document.getElementById('boxtableTwo').appendChild(newElement);
-}
-
-function boxtableThree(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table-one';
-    newElement.id = 'boxtableThree';
-    document.getElementById('boxTable').appendChild(newElement);
-}
-
-function tableThreeLeft(){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-left';
-    newElement.textContent = '教法';
-    newElement.id = 'tableThreeLeft';
-    document.getElementById('boxtableThree').appendChild(newElement);
-}
-
-function tableThreeRight(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-right';
-    newElement.textContent = data.teachWay;
-    newElement.id = 'tableThreeRight';
-    document.getElementById('boxtableThree').appendChild(newElement);
-}
-
-function boxtableFour(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table-one';
-    newElement.id = 'boxtableFour';
-    document.getElementById('boxTable').appendChild(newElement);
-}
-
-function tableFourLeft(){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-left';
-    newElement.textContent = '天數';
-    newElement.id = 'tableFourLeft';
-    document.getElementById('boxtableFour').appendChild(newElement);
-}
-
-function tableFourRight(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-right';
-    newElement.textContent = data.totalDay + '天';
-    newElement.id = 'tableFourRight';
-    document.getElementById('boxtableFour').appendChild(newElement);
-}
-
-function boxtableFive(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table-one';
-    newElement.id = 'boxtableFive';
-    document.getElementById('boxTable').appendChild(newElement);
-}
-
-function tableFiveLeft(){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-left';
-    newElement.textContent = '週時';
-    newElement.id = 'tableFiveLeft';
-    document.getElementById('boxtableFive').appendChild(newElement);
-}
-
-function tableFiveRight(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-right';
-    newElement.textContent = data.weekHour + '小時';
-    newElement.id = 'tableFiveRight';
-    document.getElementById('boxtableFive').appendChild(newElement);
-}
-
-function boxtableSix(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table-one';
-    newElement.id = 'boxtableSix';
-    document.getElementById('boxTable').appendChild(newElement);
-}
-
-function tableSixLeft(){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-left';
-    newElement.textContent = '技術';
-    newElement.id = 'tableSixLeft';
-    document.getElementById('boxtableSix').appendChild(newElement);
-}
-
-function tableSixRight(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-right';
-    newElement.textContent = data.technology;
-    newElement.id = 'tableSixRight';
-    document.getElementById('boxtableSix').appendChild(newElement);
-}
-
-function boxtableSeven(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table-one';
-    newElement.id = 'boxtableSeven';
-    document.getElementById('boxTable').appendChild(newElement);
-}
-
-function tableSevenLeft(){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-left';
-    newElement.textContent = '信箱';
-    newElement.id = 'tableSevenLeft';
-    document.getElementById('boxtableSeven').appendChild(newElement);
-}
-
-function tableSevenRight(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-right';
-    newElement.textContent = data.mail;
-    newElement.id = 'tableSevenRight';
-    document.getElementById('boxtableSeven').appendChild(newElement);
-}
-
-function boxtableEight(){
-    let newElement = document.createElement('div');
-    newElement.className = 'box-table-one';
-    newElement.id = 'boxtableEight';
-    document.getElementById('boxTable').appendChild(newElement);
-}
-
-function tableEightLeft(){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-left';
-    newElement.textContent = '電話';
-    newElement.id = 'tableEightLeft';
-    document.getElementById('boxtableEight').appendChild(newElement);
-}
-
-function tableEightRight(data){
-    let newElement = document.createElement('div');
-    newElement.className = 'table-one-right';
-    newElement.textContent = data.phone;
-    newElement.id = 'tableEightRight';
-    document.getElementById('boxtableEight').appendChild(newElement);
-}
-
 // alert 
 
 app.get("#alertBigBox").style.display = 'none';
@@ -462,15 +172,15 @@ app.get("#alertButton").addEventListener('click', ()=>{
 
 setTimeout(letLoadingNone, 1500)
 function letLoadingNone(){
-    loadingAnimation.style.height = '0px';
-    loadingAnimation.style.opacity = '0.9';
-    loadingDrawing.style.height = '0px';
-    loadingDrawing.style.opacity = '0.9';
-    loadingImg.style.marginBottom = '-1000px';
-    header.style.animation = 'headerGoUp 0.9s ease 0s 1 alternate';
-    myAside.style.animation = 'asideBottom 0.9s ease 0s 1 alternate';
+    app.get('#loadingAnimation').style.height = '0px';
+    app.get('#loadingAnimation').style.opacity = '0.9';
+    app.get('#loadingDrawing').style.height = '0px';
+    app.get('#loadingDrawing').style.opacity = '0.9';
+    app.get('#loadingImg').style.marginBottom = '-1000px';
+    app.get('#header').style.animation = 'headerGoUp 0.9s ease 0s 1 alternate';
+    app.get('#myAside').style.animation = 'asideBottom 0.9s ease 0s 1 alternate';
     setTimeout(displayNoneLoading, 600)
     function displayNoneLoading(){
-        loadingAnimation.style.display = 'none';
+        app.get('#loadingAnimation').style.display = 'none';
     }
 }
